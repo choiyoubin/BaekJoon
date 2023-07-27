cde8aef20628ac7bfd75fd482e54327e9aab00c3
Youbeenzino <48716638+choiyoubin@users.noreply.github.com>
2023-07-27 16:56:14 +0900

    [Silver II] Title: 최대 힙, Time: 164 ms, Memory: 46396 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -0,0 +1,16 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
+max_heap = []
+
+for _ in range(int(input())):
+    n = int(input())
+    
+    if n == 0:
+        if len(max_heap):
+            print(heapq.heappop(max_heap)[1])
+        else:
+            print(0)
+    else:
+        heapq.heappush(max_heap, (-n, n))
